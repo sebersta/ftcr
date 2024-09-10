8ef2536416317d18fe1c0dc9932a02f847f5c452
sebersta <jian@sebersta.com>
2024-09-09 22:12:08 -0700

    xcode version

--- ftcr Watch App/Tip.swift
+++ ftcr Watch App/Tip.swift
@@ -4,23 +4,23 @@
 //
 //  Created by Jian Qin on 2024/9/9.
 //
-import TipKit
-
-struct SingleDownloadTip: Tip {
-    var title: Text {
-        Text("Sample Image")
-    }
-    
-    var message: Text? {
-        Text("Click the buttom below to download a sample image.")
-    }
-    
-    var actions: [Action] {
-        [
-            Tip.Action(
-            id: "sample",
-            title: "Download"
-            )
-        ]
-    }
-}
+//import TipKit
+//
+//struct SingleDownloadTip: Tip {
+//    var title: Text {
+//        Text("Sample Image")
+//    }
+//    
+//    var message: Text? {
+//        Text("Click the buttom below to download a sample image.")
+//    }
+//    
+//    var actions: [Action] {
+//        [
+//            Tip.Action(
+//            id: "sample",
+//            title: "Download"
+//            )
+//        ]
+//    }
+//}
